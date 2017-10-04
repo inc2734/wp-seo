df5b2f620b1d2ad704e4ae25a10ca9a640bfea9d
Takashi Kitajima <inc@2inc.org>
2017-10-04 14:13:27 +0900

    Update google analytics tracking code

--- src/app/setup/google-analytics-tracking-id.php
+++ src/app/setup/google-analytics-tracking-id.php
@@ -20,14 +20,14 @@ add_action( 'wp_head', function() {
 		return;
 	}
 	?>
+<!-- Global Site Tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=<?php echo esc_attr( $tracking_id ); ?>"></script>
 <script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments)};
+  gtag('js', new Date());
 
-  ga('create', '<?php echo esc_js( $tracking_id ); ?>', 'auto');
-  ga('send', 'pageview');
+  gtag('config', '<?php echo esc_js( $tracking_id ); ?>');
 </script>
 	<?php
 } );
