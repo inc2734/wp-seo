7712520f88dbd2d3115077897669f52dcaac166c
Takashi Kitajima <inc@2inc.org>
2018-03-27 12:11:57 +0900

    Fix googletagmanager noscript tag

--- src/App/Setup/google-tag-manager-id.php
+++ src/App/Setup/google-tag-manager-id.php
@@ -6,7 +6,7 @@
  */
 
 /**
- * Print Google Tag Manager script
+ * Print Google Tag Manager script in head
  *
  * @return void
  */
@@ -27,3 +27,26 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 })(window,document,'script','dataLayer','<?php echo esc_js( $tag_manager_id ); ?>');</script>
 	<?php
 } );
+
+/**
+ * Print Google Tag Manager script in body
+ *
+ * @return void
+ */
+function inc2734_wp_seo_googletagmanager_noscript_tag_install() {
+	$tag_manager_id = apply_filters( 'inc2734_wp_seo_google_tag_manager_id', null );
+	if ( ! $tag_manager_id ) {
+		return;
+	}
+
+	if ( ! preg_match( '/^GTM-.+$/', $tag_manager_id ) ) {
+		return;
+	}
+	?>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=<?php echo esc_js( $tag_manager_id ); ?>"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+	<?php
+}
+add_action( 'wp_footer', 'inc2734_wp_seo_googletagmanager_noscript_tag_install' );
