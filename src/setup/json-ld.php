1806da5414109277e869698814847f483d5f5d21
inc2734 <inc@2inc.org>
2024-11-30 10:25:57 +0900

    inc2734/snow-monkey#1451

--- src/setup/json-ld.php
+++ src/setup/json-ld.php
@@ -20,64 +20,47 @@ add_action(
 		$ogp     = new \Inc2734\WP_OGP\Bootstrap();
 		$json_ld = array();
 
-		if ( is_singular() || ( is_front_page() && ! is_home() ) ) {
+		if ( is_singular() && ! is_front_page() ) {
 			if ( is_singular( 'post' ) ) {
 				$type = 'BlogPosting';
 			} else {
 				$type = 'Article';
 			}
 
-			$query = new WP_Query(
-				array(
-					'p'                   => get_the_ID(),
-					'post_type'           => 'any',
-					'posts_per_page'      => 1,
-					'ignore_sticky_posts' => true,
-					'no_found_rows'       => true,
-					'suppress_filters'    => true,
-				)
+			$json_ld = array(
+				'@context'         => 'http://schema.org',
+				'@type'            => $type,
+				'headline'         => $ogp->get_title(),
+				'author'           => array(
+					'@type' => 'Person',
+					'name'  => get_the_author(),
+				),
+				'publisher'        => array(
+					'@type' => 'Organization',
+					'url'   => home_url(),
+					'name'  => $ogp->get_site_name(),
+					'logo'  => array(
+						'@type' => 'ImageObject',
+						'url'   => wp_get_attachment_image_url( get_theme_mod( 'custom_logo' ), 'full' ),
+					),
+				),
+				'mainEntityOfPage' => array(
+					'@type' => 'WebPage',
+					'@id'   => $ogp->get_url(),
+				),
+				'image'            => array(
+					'@type' => 'ImageObject',
+					'url'   => $ogp->get_image(),
+				),
+				'datePublished'    => get_the_time( 'c' ),
+				'dateModified'     => get_the_modified_time( 'c' ),
+				'description'      => $ogp->get_description(),
 			);
 
-			while ( $query->have_posts() ) {
-				$query->the_post();
-
-				$json_ld = array(
-					'@context'         => 'http://schema.org',
-					'@type'            => $type,
-					'headline'         => $ogp->get_title(),
-					'author'           => array(
-						'@type' => 'Person',
-						'name'  => get_the_author(),
-					),
-					'publisher'        => array(
-						'@type' => 'Organization',
-						'url'   => home_url(),
-						'name'  => $ogp->get_site_name(),
-						'logo'  => array(
-							'@type' => 'ImageObject',
-							'url'   => wp_get_attachment_image_url( get_theme_mod( 'custom_logo' ), 'full' ),
-						),
-					),
-					'mainEntityOfPage' => array(
-						'@type' => 'WebPage',
-						'@id'   => $ogp->get_url(),
-					),
-					'image'            => array(
-						'@type' => 'ImageObject',
-						'url'   => $ogp->get_image(),
-					),
-					'datePublished'    => get_the_time( 'c' ),
-					'dateModified'     => get_the_modified_time( 'c' ),
-					'description'      => $ogp->get_description(),
-				);
-
-				$author_url = get_the_author_meta( 'url' );
-				if ( $author_url ) {
-					$json_ld['author']['url'] = $author_url;
-				}
+			$author_url = get_the_author_meta( 'url' );
+			if ( $author_url ) {
+				$json_ld['author']['url'] = $author_url;
 			}
-
-			wp_reset_postdata();
 		} else {
 			$json_ld = array(
 				'@context'  => 'http://schema.org',
@@ -99,7 +82,6 @@ add_action(
 		if ( ! $json_ld ) {
 			return;
 		}
-
 		?>
 		<script type="application/ld+json">
 			<?php echo wp_json_encode( $json_ld ); ?>
